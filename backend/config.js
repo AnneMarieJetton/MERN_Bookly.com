d2339e376ef3d41dace1e82875fa471a46450496
Anne Marie Jetton <annemariejetton@gmail.com>
2024-04-19 14:03:08 -0600

    initial commit, moved from previous account

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://annemariejetton:zOSbyeZoPmw68VzQ@mern-test.nmqrh5d.mongodb.net/test-collection?retryWrites=true&w=majority&appName=MERN-Test'
